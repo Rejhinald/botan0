c0652931887227d8a131bd3feb1c7d2f74eb552c
sddizon1 <sddizon1@student.hau.edu.ph>
2023-04-27 10:03:21 +0800

    pls

--- botano/src/screens/Orderscreen.js
+++ botano/src/screens/Orderscreen.js
@@ -1,59 +1,59 @@
-import React from 'react'
-import {PayPalButton} from 'react-paypal-button-v2'
+import React, { useEffect } from "react";
+import { PayPalButton } from "react-paypal-button-v2";
+import { useSelector } from "react-redux";
+import Loader from "../components/Loader";
 
 function Orderscreen() {
+  const [sdkReady, setSdkReady] = useSelector(false);
 
-    const [sdkReady, setSdkReady] = useSelector(false)
+  const addPayPalScript = () => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src =
+      "https://www.paypal.com/sdk/js?client-id=AaucYPMaRFUh-_oxSoxOl5wSEZEgHaFUfVqopKBgVd2kabPxvSmW1j6ZZymBkLq7y8ryclXS9YvVLQWwB&currency=USD";
+    script.async = true;
+    script.onload = () => {
+      setSdkReady(true);
+    };
+    document.body.appendChild(script);
+  };
 
-    const addPayPalScript = () => {
-        const script = document.createElement('script')
-        script.type = 'text/javascript'
-        script.src = 'https://www.paypal.com/sdk/js?client-id=AaucYPMaRFUh-_oxSoxOl5wSEZEgHaFUfVqopKBgVd2kabPxvSmW1j6ZZymBkLq7y8ryclXS9YvVLQWwB&currency=USD'
-        script.async = true
-        script.onload = () => {
-            setSdkReady(true)
-        }
-        document.body.appendChild(script)
+  const orderPay = useSelector((state) => state.orderPay);
+  const { loading: loadingPay, success: successPay } = orderPay;
+
+  useEffect(() => {
+    if (!order || successPay || !order._id !== Number(orderId.id)) {
+      dispatchEvent(getOrderDetails(orderId.id));
+    } else if (!order.isPaid) {
+      if (!window.paypal) {
+        addPayPalScript();
+      } else {
+        setSdkReady(true);
+      }
     }
+  }, [successPay]);
 
-    const orderPay = useSelector((state) => state.orderPay);
-    const {loading: loadingPay, success: successPay} = orderPay
+  const successPaymentHandler = (paymentResult) => {
+    dispatch(payOrder(orderId, paymentResult));
+  };
 
-    useEffect(() => {
+  {
+    !order.isPaid && (
+      <ListGroup.Item>
+        {loadingPay && <Loader />}
+        {sdkReady ? (
+          <Loader />
+        ) : (
+          <PayPalButton
+            amount={order.totalPrice}
+            onSuccess={successPaymentHandler}
+          />
+        )}
+      </ListGroup.Item>
+    );
+  }
 
-        if (!order || successPay || !order._id !== Number(orderId.id)) {
-            dispatchEvent(getOrderDetails(orderId.id));
-        }else if(!order.isPaid){
-            if(!window.paypal){
-                addPayPalScript()
-            }else{
-                setSdkReady(true)
-            }
-        }
-
-    }, [successPay]);
-
-    const successPaymentHandler = (paymentResult) => {
-        dispatch(payOrder(orderId, paymentResult))
-    }
-
-    {!order.isPaid && (
-        <ListGroup.Item>
-            {loadingPay && <Loader />}
-            {sdkReady ? (
-            <Loader /> 
-            ) : (
-                <PayPalButton
-                    amount={order.totalPrice}
-                    onSuccess={successPaymentHandler}
-                    />
-            )}
-        </ListGroup.Item>
-    )}
-
-  return (
-    <div>Orderscreen</div>
-  )
+  return <div>Orderscreen</div>;
 }
 
-export default Orderscreen
+export default Orderscreen;
