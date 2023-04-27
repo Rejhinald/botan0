2725bca9ec0205a32e3aa2e536e05e62abf0c5c8
Arwin <arwinmiclat@gmail.com>
2023-04-28 00:24:42 +0800

    last

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1by#rsmux7+&ptzc(ku(nc8826%4*_^8t^)qgsykuh#jy$=d5u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
